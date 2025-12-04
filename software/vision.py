71f14a04c23c24dc34bd2e389f1bbbc8c51b0e7f
tronghia <tronghia256@gmail.com>
2025-12-04 16:53:16 +0700

    Upload and test model

--- software/vision.py
+++ software/vision.py
@@ -1,1 +1,18 @@
-import Serial
+from ultralytics import YOLO
+import cv2
+
+model = YOLO(r"C:\Users\ASUS\Desktop\arm-robot-controller\models\best.pt")
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    results = model(frame, conf=0.5)
+    annotated = results[0].plot()
+    cv2.imshow("YOLOv8 - Webcam", annotated)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
